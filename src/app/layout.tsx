a6e66eb16d93cb2e2114f5b457ee207492d5685f
vansham-kamboj <96517860+vansham-kamboj@users.noreply.github.com>
2025-07-09 18:52:12 +0530

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -77,6 +77,7 @@ export default function RootLayout({
           href="https://fonts.googleapis.com/css2?family=Space+Grotesk:wght@400;700&display=swap"
           rel="stylesheet"
         />
+        <link rel="icon" href="/favicon.ico" />
       </head>
       <body
         className={cn('min-h-screen bg-background font-body antialiased')}
